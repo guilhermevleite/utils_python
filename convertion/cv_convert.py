29451e8ec40b538a579fcdcf04be950a92678533
Guilherme Leite <guilherme.vieira.leite@gmail.com>
2022-11-14 18:28:23 -0300

    removed nd2 converter, added nd2 converter into cv_convert.py

--- convertion/cv_convert.py
+++ convertion/cv_convert.py
@@ -9,6 +9,7 @@ https://docs.opencv.org/4.x/d4/da8/group__imgcodecs.html#ga288b8b3da0892bd651fce
 import argparse
 from pathlib import Path
 import cv2 as cv
+import nd2
 
 
 def parse_args():
@@ -26,6 +27,10 @@ def parse_args():
                          default=None,
                          help='Path to the output folder',
                          required=True)
+
+    parser.add_argument('--suffix',
+                         default='.png',
+                         help='Output extension (Default: .png)')
 
     return parser.parse_args()
 
@@ -45,21 +50,47 @@ def list_files(path: str) -> list:
     return [ x for x in Path(path).glob('*') if x.is_file() ]
 
 
-def convert_files(file_list: list, dest: str):
+def convert_cv2_formats(file_path, args):
+    '''
+    Converts image using OpenCV
+    '''
+    image = cv.imread(str(file_path))
+
+    if image is None:
+        print('ERROR: OpenCV image not loaded')
+        exit()
+
+    output_path = Path(args.output, file_path.name).with_suffix(args.suffix)
+    print(output_path.name)
+    cv.imwrite(str(output_path), image)
+
+
+def convert_nd2_format(file_path, args):
+    '''
+    Converts .nd2 images
+    '''
+    image = nd2.imread(str(file_path))
+    # image = cv.imread(str(file_path))
+
+    if image is None:
+        print('ERROR: ND2 image not loaded')
+        exit()
+
+    output_path = Path(args.output, file_path.name).with_suffix(args.suffix)
+    print(output_path.name)
+    cv.imwrite(str(output_path), image)
+
+
+def convert_files(file_list: list, args):
     '''
     Converts the files in <img_list>,
     and saves them into <dest> folder
     '''
     for file in file_list:
-        image = cv.imread(str(file))
-
-        if image is None:
-            print('ERROR: Image not read')
-            exit()
-
-        output_path = Path(dest, file.name).with_suffix('.png')
-        print(output_path.name)
-        cv.imwrite(str(output_path), image)
+        if file.suffix == '.nd2':
+            convert_nd2_format(file, args)
+        else:
+            convert_cv2_formats(file, args)
 
 
 def main():
@@ -72,7 +103,7 @@ def main():
 
     file_list = list_files(args.input)
 
-    convert_files(file_list, args.output)
+    convert_files(file_list, args)
 
 
 
