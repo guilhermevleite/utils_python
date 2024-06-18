39baa47a25f5d3ac9ec9bddd66f27cbb19567860
gui <guilherme.vieira.leite@gmail.com>
2024-06-18 15:29:33 -0300

    incomplete, just syncing
    Signed-off-by: gui <guilherme.vieira.leite@gmail.com>

--- plot/better_multi_experiments.py
+++ plot/better_multi_experiments.py
@@ -1,9 +1,15 @@
 import argparse
 from pathlib import Path
-import pandas as PD
+import pandas as pd
 import matplotlib.pyplot as PLT
 import seaborn as SBN
 
+
+config = {}
+config['sub_strings'] = [
+    'ours__GPT-test_aug-none_GPTNet',
+    'ours__GPT-test_aug-none_Unet'
+]
 
 def arg_parse():
     parser = argparse.ArgumentParser()
@@ -11,46 +17,33 @@ def arg_parse():
     parser.add_argument('-i', '--input', dest='input_path', type=str, required=True,
                         help='Path to experiments folder.')
 
+    parser.add_argument('-o', '--output', dest='output_path', type=str, required=True,
+                        help='Output path to save the results.')
+
     return parser.parse_args()
 
 
-def main():
+def adapt_df(df, exp_name, replica):
+    df.insert(-1, 'experiment', exp_name, True)
+    df.insert(-1, 'replica', replica, True)
+
+
+if __name__ == "__main__":
 
     args = arg_parse()
 
-    path = Path(args.input_path).glob('./*')
-    # SBN.set_theme()
-    exp_names = []
-    df = None
+    for experiment in config['sub_strings']:
 
-    ## OO style of Matplot, uses pyplot just to create the figure
-    fig, ax = PLT.subplots()
+        print(f'Gathering information about {experiment}')
+        replicates = Path(args.input_path).glob(f'{experiment}*')
+        result_list = []
 
-    for idx, experiment in enumerate(sorted(path)):
-        csv_path = Path(experiment) / 'log.csv'
+        for idx, replica in enumerate(replicates):
 
-        if not experiment.is_dir():
-            continue
+            csv = pd.read_csv(Path(replica) / 'log.csv')
+            output = csv[['epoch', 'val_dice']]
+            adapt_df(csv, experiment, idx)
+            result_list.append(output)
 
-        try:
-            df = PD.read_csv(csv_path)
-        except:
-            print('Failed', experiment.name)
-            continue
-
-        ax.plot(df['val_dice'], label=experiment.name)
-
-        # if idx == 2:
-        #     break
-
-    ax.set_xlabel('Epochs')
-    ax.set_ylabel('Dice')
-    ax.legend()
-
-    PLT.xlim([0, 100])
-    PLT.ylim([0.85, 1.0])
-    PLT.savefig(Path(args.input_path) / 'Experiments.png')
-
-
-if __name__ == '__main__':
-    main()
+        print(len(result_list))
+        print(result_list)
